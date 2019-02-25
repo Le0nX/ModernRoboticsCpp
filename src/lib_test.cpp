0cfd0184924c04785b175577db9ecbb911753aae
sunoval2016 <wenwuxiu1106@gmail.com>
2019-02-25 05:59:34 -0500

    Add EulerStep, ComputedTorque and corresponding gtest

--- src/lib_test.cpp
+++ src/lib_test.cpp
@@ -689,3 +689,98 @@ TEST(MRTest, ForwardDynamicsTest) {
 
   ASSERT_TRUE(ddthetalist.isApprox(result, 4));
 }
+
+TEST(MRTest, EulerStepTest) {
+	Eigen::VectorXd thetalist(3);
+	thetalist << 0.1, 0.1, 0.1;
+	Eigen::VectorXd dthetalist(3);
+	dthetalist << 0.1, 0.2, 0.3;
+	Eigen::VectorXd ddthetalist(3);
+	ddthetalist << 2, 1.5, 1;
+	double dt = 0.1;
+
+	mr::EulerStep(thetalist, dthetalist, ddthetalist, dt);
+
+	Eigen::VectorXd result_thetalistNext(3);
+	result_thetalistNext << 0.11, 0.12, 0.13;
+	Eigen::VectorXd result_dthetalistNext(3);
+	result_dthetalistNext << 0.3, 0.35, 0.4;
+
+	ASSERT_TRUE(thetalist.isApprox(result_thetalistNext, 4));
+	ASSERT_TRUE(dthetalist.isApprox(result_dthetalistNext, 4));
+}
+
+TEST(MRTest, ComputedTorqueTest) {
+	Eigen::VectorXd thetalist(3);
+	thetalist << 0.1, 0.1, 0.1;
+	Eigen::VectorXd dthetalist(3);
+	dthetalist << 0.1, 0.2, 0.3;
+	Eigen::VectorXd eint(3);
+	eint << 0.2, 0.2, 0.2;
+	Eigen::VectorXd g(3);
+	g << 0, 0, -9.8;
+
+	std::vector<Eigen::MatrixXd> Mlist;
+	std::vector<Eigen::MatrixXd> Glist;
+
+	Eigen::Matrix4d M01;
+	M01 << 1, 0, 0, 0,
+		0, 1, 0, 0,
+		0, 0, 1, 0.089159,
+		0, 0, 0, 1;
+	Eigen::Matrix4d M12;
+	M12 << 0, 0, 1, 0.28,
+		0, 1, 0, 0.13585,
+		-1, 0, 0, 0,
+		0, 0, 0, 1;
+	Eigen::Matrix4d M23;
+	M23 << 1, 0, 0, 0,
+		0, 1, 0, -0.1197,
+		0, 0, 1, 0.395,
+		0, 0, 0, 1;
+	Eigen::Matrix4d M34;
+	M34 << 1, 0, 0, 0,
+		0, 1, 0, 0,
+		0, 0, 1, 0.14225,
+		0, 0, 0, 1;
+
+	Mlist.push_back(M01);
+	Mlist.push_back(M12);
+	Mlist.push_back(M23);
+	Mlist.push_back(M34);
+
+	Eigen::VectorXd G1(6);
+	G1 << 0.010267, 0.010267, 0.00666, 3.7, 3.7, 3.7;
+	Eigen::VectorXd G2(6);
+	G2 << 0.22689, 0.22689, 0.0151074, 8.393, 8.393, 8.393;
+	Eigen::VectorXd G3(6);
+	G3 << 0.0494433, 0.0494433, 0.004095, 2.275, 2.275, 2.275;
+
+	Glist.push_back(G1.asDiagonal());
+	Glist.push_back(G2.asDiagonal());
+	Glist.push_back(G3.asDiagonal());
+
+	Eigen::MatrixXd SlistT(3, 6);
+	SlistT << 1, 0, 1, 0, 1, 0,
+		0, 1, 0, -0.089, 0, 0,
+		0, 1, 0, -0.089, 0, 0.425;
+	Eigen::MatrixXd Slist = SlistT.transpose();
+
+	Eigen::VectorXd thetalistd(3);
+	thetalistd << 1.0, 1.0, 1.0;
+	Eigen::VectorXd dthetalistd(3);
+	dthetalistd << 2, 1.2, 2;
+	Eigen::VectorXd ddthetalistd(3);
+	ddthetalistd << 0.1, 0.1, 0.1;
+	double Kp = 1.3;
+	double Ki = 1.2;
+	double Kd = 1.1;
+
+	Eigen::VectorXd taulist = mr::ComputedTorque(thetalist, dthetalist, eint, g,
+		Mlist, Glist, Slist, thetalistd, dthetalistd, ddthetalistd, Kp, Ki, Kd);
+
+	Eigen::VectorXd result(3);
+	result << 133.00525246, -29.94223324, -3.03276856;
+
+	ASSERT_TRUE(taulist.isApprox(result, 4));
+}
